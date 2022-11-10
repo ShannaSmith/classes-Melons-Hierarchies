e9fd61f2e164523792f2614c4e09124629720e99
ShannaSmith <shannagjh@gmail.com>
2022-11-10 16:55:23 -0500

    correct code refactor part 1

--- melons.py
+++ melons.py
@@ -5,69 +5,41 @@ class AbstractMelonOrder:
         self.qty=qty
         self.shipped = False
         
-def get_total(self):
+    def get_total(self):
         """Calculate price, including tax."""
 
         base_price = 5
         total = (1 + self.tax) * self.qty * base_price
 
         return total
-def mark_shipped(self):
-    """Record the fact than an order has been shipped."""
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
 
-    self.shipped = True
+        self.shipped = True
     
 
-class DomesticMelonOrder:
+class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
-
+     
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
-        shipped = False
-
-        #self.shipped = False 
+        super().__init__(species, qty)
         self.order_type = "domestic"
         self.tax = 0.08
 
-    # #def get_total(self):
-    #     """Calculate price, including tax."""
 
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
-
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Record the fact than an order has been shipped."""
-
-    #     self.shipped = True
-
-
-class InternationalMelonOrder:
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order() attributes."""
-        shipped = False
+        super().__init__(species, qty)
       
         self.country_code = country_code
-       # self.shipped = False
         self.order_type = "international"
         self.tax = 0.17
 
-    # def get_total(self):
-    #     """Calculate price, including tax."""
-
-    #     base_price = 5
-    #     total = (1 + self.tax) * self.qty * base_price
-
-    #     return total
-
-    # def mark_shipped(self):
-    #     """Record the fact than an order has been shipped."""
-
-    #     self.shipped = True
-
+    
     def get_country_code(self):
         """Return the country code."""
 
